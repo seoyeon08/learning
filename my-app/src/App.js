5a39f749ecbe2457431f3b3fdf953312f80ff90b
seoyeon <silversun0808@gmail.com>
2021-01-27 15:15:01 +0900

    first commit of comment

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Comment from './Comment';
+
+function App() {
+  return (
+    <div className="App" style={{padding: 16, backgroundColor: '#282c34'}}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div>
+        <Comment />
+      </div>
+    </div>
+  );
+}
+
+export default App;
