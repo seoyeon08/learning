42f50694d9b673640a203dd4c62708678d0423b3
seoyeon <silversun0808@gmail.com>
2021-01-28 23:10:07 +0900

    modify comment.js

--- my-app/src/Comment.js
+++ my-app/src/Comment.js
@@ -37,6 +37,7 @@ const styles = {
 }
 class Comment extends React.Component {
     render(){
+        const {name, content} = this.props;
         return (
             <div style={styles.root}>
                 <div style={styles.imageContainer}>
@@ -46,10 +47,10 @@ class Comment extends React.Component {
                 </div>
                 <div style={styles.commentContainer}>
                     <div style={styles.nameText}>
-                        {this.props.name}
+                        {name}
                     </div>
                     <span style={styles.contentText}>
-                        {this.props.content}
+                        {content}
                     </span>
                 </div>
             </div>
