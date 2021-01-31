948703010563c6efbf3d12559e0b7b649eb3d6c9
seoyeon <silversun0808@gmail.com>
2021-01-31 15:11:32 +0900

    arrow function

--- my-app/src/Chapter06.js
+++ my-app/src/Chapter06.js
@@ -5,12 +5,9 @@ class Toggle extends React.Component {
     constructor(props) {
         super(props);
         this.state = {isToggleOn: true};
-
-        //This binding is necessary to make 'this' work in the callback
-        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick(){
+    handleClick = () => {
         this.setState(state => ({
             isToggleOn: !state.isToggleOn
         }));
