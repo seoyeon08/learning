6d35babbeb69901b4e823610bf32c6c3de22d064
seoyeon <silversun0808@gmail.com>
2021-02-03 17:48:23 +0900

    end signUp.js

--- my-app/src/Chapter09/SignUp.js
+++ my-app/src/Chapter09/SignUp.js
@@ -5,18 +5,20 @@ class SignUp extends React.Component {
         super(props);
 
         this.state={
-            name: ''
+            name: '',
+            gender: '1',
         }
     }
 
     handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
+
         });
     }
 
     render() {
-        var {name} = this.state;
+        var {name, gender} = this.state;
 
         return(
             <form onSubmit={this.handleSubmit}>
@@ -28,6 +30,18 @@ class SignUp extends React.Component {
                         value={name}
                         onChange={this.handleChange}/>
                 </label>
+                <br />
+                <label>
+                    Gender
+                    <select
+                        name={'gender'}
+                        value={gender}
+                        onChange={this.handleChange}>
+                        <option value={1}>Man</option>
+                        <option value={0}>Woman</option>
+                    </select>
+                </label>
+                <br />
                 <input type='submit' value='Submit' />
             </form>
         )
